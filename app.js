182ca4dd4b165f08ebee516f80ee4c6a2283b939
Yongyuan Ma <yongyuan.ma43@qmail.cuny.edu>
2020-07-01 15:30:57 -0400

    adding bcrypt package

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ const mongoose=require("mongoose");
 //MD5 hashing
 const md5=require("md5");
 //salting and hashing
-// const bcrypt=require("bcrypt");
-// //salt round
-// const saltRounds=10;
+const bcrypt=require("bcrypt");
+//salt round
+const saltRounds=10;
 const app=express();
 // const encrypt=require("mongoose-encryption");
 
@@ -42,34 +42,37 @@ app.get("/register",function(req,res){
 })
 
 app.post("/register",function(req,res){
-//   bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash) {
-//     // Store hash in your password DB.
-// });
-  const newUser=new User({
-    email:req.body.username,
-    password:md5(req.body.password)
-  });
-  newUser.save(function(err){
-    if(err){
-      console.log(err);
-    }else{
-      res.render("secrets");
-    }
-  })
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser=new User({
+      email:req.body.username,
+      password:hash
+    });
+    newUser.save(function(err){
+      if(err){
+        console.log(err);
+      }else{
+        res.render("secrets");
+      }
+    })
+});
+
 
 })
 
 app.post("/login",function(req,res){
   const username=req.body.username;
-  const password=md5(req.body.password);
+  const password=req.body.password;
   User.findOne({email:username},function(err,foundUser){
     if(err){
       console.log("No User Found");
     }else{
       if(foundUser){
-        if(foundUser.password===password){
-          res.render("secrets");
-        }
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+      // result == true
+      if(result===true){
+      res.render("secret");
+    }
+  });
       }
     }
   })
